0bf94f088930e5415a6095b90271fa767630dba6
mitya-storozhuk <88045766+mitya-storozhuk@users.noreply.github.com>
2021-09-28 23:05:12 +0300

    Update task1.js

--- task1.js
+++ task1.js
@@ -44,7 +44,9 @@ const first = arr.shift();
 console.log(arr);
 
 // task4
-
+let cities = ["Rome", "Lviv", "Warsaw"]; 
+let newCities = cities.join('*');
+console.log(newCities);
 
 // task5
 const isAdult = promt ("Ви досягли повнолітнього віку?", "0");
@@ -53,3 +55,52 @@ if (isAdult > 0 && isAdult < 18) {
 } else if (isAdult >= 18) {
     console.log ("Ви досягли повнолітнього віку");
 } else console.log ("Ви ввели не коректні данні");
+
+// task6
+const sideA = promt ("Введіть сторону А трикутника", "3");
+const sideB = promt ("Введіть сторону А трикутника", "4");
+const sideC = promt ("Введіть сторону А трикутника", "5");
+const perim = (sideA + sideB + sideC) * (1 / 2);
+const triangleSquare = (perim * (perim - sideA) * (perim - sideB) * (perim - sideC)) ** (1 / 2);
+const triangleSquareRight = sideA * sideB  * (1 / 2);
+
+if ( ((sideA + sideB) < sideC) || ((sideA + sideC) < sideB) || ((sideB + sideC) < sideA) || (sideA < 0) || (sideB < 0) || (sideC < 0)) {
+    alert("Incorrect data");
+};
+if (triangleSquare === triangleSquareRight) {
+    console.log("Трикутник є прямокутним");
+};
+if (sideC ** 2 === sideA ** 2 + sideB ** 2) {
+    console.log("Трикутник є прямокутним");
+};
+
+console.log(triangleSquare.toFixed(3));
+
+// task7
+let time = new Date();
+let hour = time.getHours();
+
+if (hour > 22 && hour < 5) {
+    console.log("Доброї ночі");
+} else if (hour >= 5 && hour <= 11) {
+    console.log("Доброго ранку");
+} else if (hour > 11 && hour <= 17) {
+    console.log("Доброго дня");
+} else {
+    console.log("Доброго вечора");
+};
+
+let hours = time.getHours();
+switch (true) {
+     case (hours >= 23 || hours <= 5):
+        console.log("Доброї ночі");
+        break;
+    case (hours > 5 || hours < 11):
+        console.log("Доброго ранку");
+        break;
+    case (hours > 11 || hours < 17):
+        console.log("Доброго дня");
+        break;
+    default:
+        console.log("Доброго вечора");
+};
